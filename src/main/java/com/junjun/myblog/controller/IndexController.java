12a1d6d8e7045a66797be370615c91ff613f4532
shinyjunjun <1273365548@qq.com>
2017-04-07 14:54:38 +0800

    myblog commit 4 add pagehelper

--- src/main/java/com/junjun/myblog/controller/IndexController.java
+++ src/main/java/com/junjun/myblog/controller/IndexController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.junjun.myblog.domain.Blog;
 import com.junjun.myblog.domain.BlogType;
 import com.junjun.myblog.domain.Blogger;
@@ -39,22 +42,28 @@ public class IndexController {
 	
 
 	@RequestMapping("/index")
-	public String index(Model model) {
+	public String index(
+			@RequestParam(required = true,defaultValue="1") Integer pageNum,
+			@RequestParam(required = true,defaultValue="5") Integer pageSize,
+			Model model) {
 
 		Blogger blogger = bloggerService.findByUsername("yuzhijun");// 因为只是自已是主人，所以直接传入名字
 		List<BlogType> blogTypeList = blogTypeService.getBlogTypeData();//获取文章类型信息
 		List<Blog> blogArchiveList = blogService.getBlogArchive();//获取文章归档信息
-		List<Blog> blogArticleList = blogService.getBlogArticle();//获取文章主题信息
 		
+		PageHelper.startPage(pageNum,pageSize);// 默认从第一页开始，每页五条
+		List<Blog> blogArticleList = blogService.getBlogArticle();//获取文章主题信息
+		PageInfo<Blog> pageBlog = new PageInfo<Blog>(blogArticleList);//将查出来的blogArticleList绑定到pageInfo中	
 		System.out.println("个人资料: "+blogger);
 		System.out.println("文章类型: "+blogTypeList);
 		System.out.println("文章归档: "+blogArchiveList);
-		System.out.println("文章主题: "+blogArticleList);
+		System.out.println("文章主体: "+blogArticleList);
 		
 		model.addAttribute("blogger", blogger);// 存入个人资料
 		model.addAttribute("blogTypeList", blogTypeList);//存入文章类型
 		model.addAttribute("blogArchiveList", blogArchiveList);//存入文章归档
 		model.addAttribute("blogArticleList", blogArticleList);//存入文章主题
+		model.addAttribute("pageBlog",pageBlog);//设置pageBlog属性
 		
 		return "index";
 	}
